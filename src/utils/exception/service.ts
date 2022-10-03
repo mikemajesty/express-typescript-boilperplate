dea7f0eb923bf1ee70188ae5891715f8498ee48f
Mike Lima <mike.rodrigues.lima@gmail.com>
2022-10-03 17:13:33 -0300

    (feat) add exception

---
+++ src/utils/exception/service.ts
@@ -0,0 +1,21 @@
+import { HttpStatus } from './status';
+
+type Response = any;
+
+export class ApiException extends Error {
+  readonly config?: unknown;
+  readonly code?: string;
+  statusCode: number | undefined;
+
+  constructor(error: Response, status?: HttpStatus, private readonly context?: string) {
+    const err = [error?.message, error].find(Boolean);
+    super(err);
+    this.message = err;
+    this.name = ApiException.name;
+    this.statusCode = [status, Number(this.code), HttpStatus.INTERNAL_SERVER_ERROR].find(Boolean);
+
+    if (this.context) {
+      this.context = this.context;
+    }
+  }
+}
