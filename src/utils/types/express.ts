24b8d13a9fb76d94b5c5028e2dc790b90ee935c5
Mike Lima <mike.rodrigues.lima@gmail.com>
2022-10-06 19:59:11 -0300

    (feat) add express utils types

---
+++ src/utils/types/express.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { IInfra } from '@/interfaces/infra';
+
+export { Router } from 'express';
+
+export type ApiRequest = Request & { infra: IInfra };
+export type ApiResponse = Response;
+export type ApiNextFunction = NextFunction;
